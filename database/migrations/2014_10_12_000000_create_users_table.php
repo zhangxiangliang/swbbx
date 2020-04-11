34c91662b92104a3a695865a2d81657165ab6dbc
zhangxiangliang <admin@pushme.top>
2020-04-12 01:21:18 +0800

    feat: 更新用户数据表和增加话题数据表

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,14 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+
+            // 用户数据
+            $table->string('avatar')->nullable();
+            $table->string('introduction')->nullable();
+
+            // 时间线
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
         });
     }
 
