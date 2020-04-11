05dc2fad6ce6b3461c00a4f70272ea7d4152324f
zhangxiangliang <admin@pushme.top>
2020-04-11 21:01:04 +0800

    feat: 增加中文本地化相关

--- config/app.php
+++ config/app.php
@@ -158,13 +158,13 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
         /*
          * Package Service Providers...
          */
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
 
         /*
          * Application Service Providers...
