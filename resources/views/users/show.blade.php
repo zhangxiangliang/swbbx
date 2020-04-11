470c268b7bd112c2bea6a66e87e69c29649edd61
zhangxiangliang <admin@pushme.top>
2020-04-11 21:52:39 +0800

    feat: 更新视图文件中关于用户头像的部分

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,7 +8,7 @@
 
   <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
     <div class="card ">
-      <img class="card-img-top" src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="{{ $user->name }}">
+      <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
       <div class="card-body">
         <h5><strong>个人简介</strong></h5>
         <p>{{ $user->introduction }}</p>
@@ -21,7 +21,7 @@
   <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
     <div class="card ">
       <div class="card-body">
-          <h1 class="mb-0" style="font-size:22px;">{{ $user->name }}</h1>
+        <h1 class="mb-0" style="font-size:22px;">{{ $user->name }}</h1>
       </div>
     </div>
     <hr>
