2541e5d39f7f2d4b30f9cf1a158d76fd3f1c450e
zhangxiangliang <admin@pushme.top>
2020-04-12 01:27:08 +0800

    feat: 增加话题路由和控制器相关

--- routes/web.php
+++ routes/web.php
@@ -36,5 +36,6 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
-// 用户相关路由
+// 相关路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
