cb9e4bdf05287745fce65b9d5f847663e68dc65f
zhangxiangliang <admin@pushme.top>
2020-04-12 02:37:41 +0800

    feat: 增加摘要帮助函数

--- app/helpers.php
+++ app/helpers.php
@@ -3,8 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 if (!function_exists('route_class')) {
+    /**
+     * 获取当前路由类名
+     * @return string
+     */
     function route_class()
     {
         return str_replace('.', '-', Route::currentRouteName());
+    }
+}
+
+if (!function_exists('make_excerpt')) {
+    /**
+     * 获取内容摘要
+     * @param string $value 摘要
+     * @param string $length 长度限制
+     * @return string
+     */
+    function make_excerpt($value, $length = 200)
+    {
+        $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+        return Str::limit($excerpt, $length);
     }
 }
