f7faa9be9b65de3608d477fccd93716a4784d827
zhangxiangliang <admin@pushme.top>
2020-04-11 21:54:24 +0800

    fix: 修复首页 $user 变量不存在导致的错误

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,8 @@
 <footer class="footer">
   <div class="container">
     <p class="float-left">
-      由 <a href="https://tieba.baidu.com/home/main?id=tb.1.48b661b8.AwkFnnkf2MbNJ_NsssUhVg" target="_blank">張小二</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+      由 <a href="https://tieba.baidu.com/home/main?id=tb.1.48b661b8.AwkFnnkf2MbNJ_NsssUhVg" target="_blank">張小二</a>
+      设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
   </div>
 </footer>
