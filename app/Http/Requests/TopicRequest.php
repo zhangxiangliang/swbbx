6c6028640aad960c77fcc79e2fa388c65254546d
zhangxiangliang <admin@pushme.top>
2020-04-12 02:37:24 +0800

    feat: 完善话题功能

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -12,17 +12,14 @@ class TopicRequest extends Request
             case 'POST':
             {
                 return [
-                    // CREATE ROLES
+                    'body' => 'required|min:3',
+                    'title' => 'required|min:2',
+                    'category_id' => 'required|numeric',
                 ];
             }
             // UPDATE
             case 'PUT':
             case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
             case 'GET':
             case 'DELETE':
             default:
@@ -35,7 +32,8 @@ class TopicRequest extends Request
     public function messages()
     {
         return [
-            // Validation messages
+            'title.min' => '标题必须至少两个字符',
+            'body.min' => '文章内容必须至少三个字符',
         ];
     }
 }
