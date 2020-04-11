6f6265e9b0d4beae906692c47fa4dceda0394534
zhangxiangliang <admin@pushme.top>
2020-04-12 02:03:48 +0800

    fix: 头部导航栏显示错误

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -13,7 +13,6 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
       <ul class="navbar-nav mr-auto">
-        {{ Route::currentRouteName() }}
         <li class="nav-item {{request()->is('topics') ? 'active': ''}}">
           <a class="nav-link" href="{{ route('topics.index') }}">话题</a>
         </li>
