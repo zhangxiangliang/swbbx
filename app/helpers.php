39392d1045fdc92031fd4df031c3644c4bc3b88a
zhangxiangliang <admin@pushme.top>
2020-04-11 20:29:46 +0800

    feat: 增加 route_class 帮助函数

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,10 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+if (!function_exists('route_class')) {
+    function route_class()
+    {
+        return str_replace('.', '-', Route::currentRouteName());
+    }
+}
