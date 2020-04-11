f05a2204a050a764d0f9fb852c2bdd498e6d45b1
zhangxiangliang <admin@pushme.top>
2020-04-12 01:26:45 +0800

    feat: 更新用户和话题验证规则

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -3,9 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Foundation\Http\FormRequest;
 
-class UserRequest extends FormRequest
+class UserRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
