d485ac7b6cd50dd693c5b37f8a13bb9e22f2dcb1
zhangxiangliang <admin@pushme.top>
2020-04-11 20:28:55 +0800

    feat: 增加页面基础模板

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://tieba.baidu.com/home/main?id=tb.1.48b661b8.AwkFnnkf2MbNJ_NsssUhVg" target="_blank">張小二</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+  </div>
+</footer>
