7f665911392b06902f4ef7f63732a26385442f25
zhangxiangliang <admin@pushme.top>
2020-04-12 01:25:20 +0800

    feat: 更新用户和话题权限管理

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
+        \App\Models\User::class => \App\Policies\UserPolicy::class,
     ];
 
     /**
@@ -27,8 +28,8 @@ class AuthServiceProvider extends ServiceProvider
 
         // 修改策略自动发现的逻辑
         // 动态返回模型对应的策略名称，如：'App\Model\User' => 'App\Policies\UserPolicy',
-        Gate::guessPolicyNamesUsing(function ($modelClass) {
-            return 'App\Policies\\'.class_basename($modelClass).'Policy';
-        });
+        // Gate::guessPolicyNamesUsing(function ($modelClass) {
+        //     return 'App\Policies\\'.class_basename($modelClass).'Policy';
+        // });
     }
 }
