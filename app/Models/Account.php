b8b2a44033198a63a1d0955b2899d0edcfdf1c82
zhangxiangliang <admin@pushme.top>
2020-04-22 22:07:22 +0800

    feat: 增加账号模型和数据表

---
+++ app/Models/Account.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+class Account extends Model
+{
+    protected $fillable = [
+        'account', 'sex', 'username',
+    ];
+}
---
+++ database/migrations/2020_04_21_222315_create_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('account')->default(0)->comment('战盟id');
+            $table->integer('sex')->default(0)->comment('性别');
+            $table->string('username')->default('')->comment('用户名');
+
+            // 时间线
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
