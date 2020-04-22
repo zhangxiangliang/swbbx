f7cf66c215c859bde7d2c6cba9e5f88bcd596561
zhangxiangliang <admin@pushme.top>
2020-04-22 23:42:25 +0800

    feat: 增加门派技能表和模型

--- database/migrations/2020_04_22_222428_create_schools_table.php
+++ database/migrations/2020_04_22_222428_create_schools_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAccountsTable extends Migration
+class CreateSchoolsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,16 @@ class CreateAccountsTable extends Migration
      */
     public function up()
     {
-        Schema::create('accounts', function (Blueprint $table) {
+        Schema::create('schools', function (Blueprint $table) {
             // 标识
             $table->id();
+            $table->integer('npc_id')->default(0)->comment('NPC id');
+            $table->integer('map_id')->default(0)->comment('地图 id');
 
             // 数据结构
-            $table->integer('account')->default(0)->comment('战盟id');
-            $table->integer('sex')->default(0)->comment('性别');
-            $table->string('username')->default('')->comment('用户名');
+            $table->string('name')->default('')->comment('名称');
+            $table->string('intro')->default('')->comment('简介');
+            $table->string('description')->default('')->comment('描述');
 
             // 时间线
             $table->dateTime('created_at');
@@ -35,6 +37,6 @@ class CreateAccountsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('accounts');
+        Schema::dropIfExists('schools');
     }
 }
