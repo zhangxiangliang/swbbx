408d7376b8cd7c9fc59f712604b46ac813674dd7
zhangxiangliang <admin@pushme.top>
2020-04-23 01:40:13 +0800

    feat: 重构门派数据合并到地图模型

--- app/Models/Map.php
+++ app/Models/Map.php
@@ -4,5 +4,13 @@ namespace App\Models;
 
 class Map extends Model
 {
-    //
+    public function teacher()
+    {
+        return $this->belongsTo(Npc::class);
+    }
+
+    public function npcs()
+    {
+        return $this->hasMany(Npc::class);
+    }
 }
