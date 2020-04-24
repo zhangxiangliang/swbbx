e49b8a8af828a1b69596cbeca88cada5dda9c54c
zhangxiangliang <admin@pushme.top>
2020-04-25 02:34:52 +0800

    feat: 增加友情链接

--- resources/views/shared/_platform.blade.php
+++ resources/views/shared/_platform.blade.php
@@ -8,3 +8,23 @@
     <li class="list-group-item">【PC端】小程序转发到电脑微信</li>
   </ul>
 </div>
+
+<div class="card card--color mt-2">
+  <div class="card-header bg-transparent">
+    友情链接
+  </div>
+  <ul class="list-group list-group-flush">
+    <div class="list-group-item">
+      <div class="d-flex align-items-center justify-content-between">
+        <div>小二の魔法实验室</div>
+        <a href="http://www.pushme.top" class="badge badge-success">查看</a>
+      </div>
+    </div>
+    <div class="list-group-item">
+      <div class="d-flex align-items-center justify-content-between">
+        <div>指数百宝箱</div>
+        <a href="http://zsbbx.pushme.top" class="badge badge-success">查看</a>
+      </div>
+    </div>
+  </ul>
+</div>
