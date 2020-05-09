4205b2810c3d1e73f50bae1e21e6ada5b158c313
zhangxiangliang <admin@pushme.top>
2020-05-09 20:27:47 +0800

    feat: 更新称谓视图

--- resources/views/titles/_item.blade.php
+++ resources/views/titles/_item.blade.php
@@ -1,16 +1,21 @@
 <div class="card">
   <div class="list-group list-group-flush">
     <div class="list-group-item">
-      {{$title->name}}
+      <div>{{$title->name}}</div>
     </div>
-    @if($title->npc)
     <div class="list-group-item">
+      @if(count($title->tags))
+      @foreach ($title->tags as $tag)
+      <div class="badge badge-secondary">{{$tag}}</div>
+      @endforeach
+      @endif
+      @if($title->npc)
       <a href="{{route('maps.show', $title->map->id)}}" class="badge badge-success">{{$title->map->name}}</a>
       <a href="{{route('npcs.show', $title->npc->id)}}" class="badge badge-success">{{$title->npc->name}}</a>
+      @endif
     </div>
-    @endif
     <div class="list-group-item">
-      描述: {{ $title->description }}
+      说明: {{ $title->description }}
     </div>
   </div>
 </div>
