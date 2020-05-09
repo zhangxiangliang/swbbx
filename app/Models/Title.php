6e63eaa0821b0cd6b3bf9b79d23248ad2b4683cf
zhangxiangliang <admin@pushme.top>
2020-05-09 20:27:39 +0800

    feat: 更新称谓数据结构

--- app/Models/Title.php
+++ app/Models/Title.php
@@ -4,6 +4,12 @@ namespace App\Models;
 
 class Title extends Model
 {
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+        'tags' => 'json',
+    ];
+
     public function npc()
     {
         return $this->belongsTo(Npc::class);
